975c77ede30cb9a282348e6db9429ac49fe5474c
valeragashi <vg70223@ubt-uni.net>
2025-01-31 00:12:11 +0100

    photo & product added

---
+++ admin/photo-delete.php
@@ -0,0 +1,36 @@
+<?php require_once('header.php'); ?>
+
+<?php
+if(!isset($_REQUEST['id'])) {
+	header('location: logout.php');
+	exit;
+} else {
+	
+	$statement = $pdo->prepare("SELECT * FROM tbl_photo WHERE id=?");
+	$statement->execute(array($_REQUEST['id']));
+	$total = $statement->rowCount();
+	if( $total == 0 ) {
+		header('location: logout.php');
+		exit;
+	}
+}
+	
+
+$statement = $pdo->prepare("SELECT * FROM tbl_photo WHERE id=?");
+$statement->execute(array($_REQUEST['id']));
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
+foreach ($result as $row) {
+	$photo = $row['photo'];
+}
+
+
+if($photo!='') {
+	unlink('../assets/uploads/'.$photo);
+}
+
+
+$statement = $pdo->prepare("DELETE FROM tbl_photo WHERE id=?");
+$statement->execute(array($_REQUEST['id']));
+
+header('location: photo.php');
+?>
---
+++ admin/product-delete.php
@@ -0,0 +1,73 @@
+<?php require_once('header.php'); ?>
+
+<?php
+if(!isset($_REQUEST['id'])) {
+	header('location: logout.php');
+	exit;
+} else {
+	
+	$statement = $pdo->prepare("SELECT * FROM tbl_product WHERE p_id=?");
+	$statement->execute(array($_REQUEST['id']));
+	$total = $statement->rowCount();
+	if( $total == 0 ) {
+		header('location: logout.php');
+		exit;
+	}
+}
+?>
+
+<?php
+	
+	$statement = $pdo->prepare("SELECT * FROM tbl_product WHERE p_id=?");
+	$statement->execute(array($_REQUEST['id']));
+	$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
+	foreach ($result as $row) {
+		$p_featured_photo = $row['p_featured_photo'];
+		unlink('../assets/uploads/'.$p_featured_photo);
+	}
+
+	
+	$statement = $pdo->prepare("SELECT * FROM tbl_product_photo WHERE p_id=?");
+	$statement->execute(array($_REQUEST['id']));
+	$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
+	foreach ($result as $row) {
+		$photo = $row['photo'];
+		unlink('../assets/uploads/product_photos/'.$photo);
+	}
+
+
+	
+	$statement = $pdo->prepare("DELETE FROM tbl_product WHERE p_id=?");
+	$statement->execute(array($_REQUEST['id']));
+
+	
+	$statement = $pdo->prepare("DELETE FROM tbl_product_photo WHERE p_id=?");
+	$statement->execute(array($_REQUEST['id']));
+
+	
+	$statement = $pdo->prepare("DELETE FROM tbl_product_size WHERE p_id=?");
+	$statement->execute(array($_REQUEST['id']));
+
+	
+	$statement = $pdo->prepare("DELETE FROM tbl_product_color WHERE p_id=?");
+	$statement->execute(array($_REQUEST['id']));
+
+	
+	$statement = $pdo->prepare("DELETE FROM tbl_rating WHERE p_id=?");
+	$statement->execute(array($_REQUEST['id']));
+
+	
+	$statement = $pdo->prepare("SELECT * FROM tbl_order WHERE product_id=?");
+	$statement->execute(array($_REQUEST['id']));
+	$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
+	foreach ($result as $row) {
+		$statement1 = $pdo->prepare("DELETE FROM tbl_payment WHERE payment_id=?");
+		$statement1->execute(array($row['payment_id']));
+	}
+
+	
+	$statement = $pdo->prepare("DELETE FROM tbl_order WHERE product_id=?");
+	$statement->execute(array($_REQUEST['id']));
+
+	header('location: product.php');
+?>
